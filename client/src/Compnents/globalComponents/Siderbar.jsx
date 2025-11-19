a6a845dd0d048acddb4a30c98cb8b3b7eb56f583
Roop Teja <roopteja52@gmail.com>
2025-11-19 19:32:54 +0530

    updated admin dashboard

--- client/src/Compnents/globalComponents/Siderbar.jsx
+++ client/src/Compnents/globalComponents/Siderbar.jsx
@@ -10,54 +10,44 @@ import Dashboard_Icon_Clicked from "../../assets/Images/Dashboard_Icon_Clicked.p
 const Sidebar = () => {
   return (
     <>
-      <div className="min-h-screen flex flex-col bg-[#FFFBF4]">
+      <div className="min-h-screen flex flex-col">
         <Header />
 
         <div className="flex flex-1 overflow-hidden">
-          {/*Side Bar Layout */}
-          <div className="w-60 bg-[#FFFBF4] border-r-2 border-gray-300 p-4 space-y-4 flex flex-col">
-            <NavLink
-              to="/admin/dashboard"
-              className={({ isActive }) =>
-                `flex items-center space-x-2 px-4 py-2 rounded-md ${
-                  isActive
-                    ? "bg-[#cfedac] text-[#7B9D51]"
-                    : "text-black bg-gray-200"
-                }`
-              }
-            >
-              {({ isActive }) => (
-                <>
-                  <img
-                    src={
-                      isActive
-                        ? Dashboard_Icon_Clicked
-                        : Dashboard_Icon_NotClicked
-                    }
-                    className="h-5"
-                  />
-                  <p> DashBoard </p>
-                </>
-              )}
-            </NavLink>
-            <Link to="/admin/content-management">Content Management</Link>
-            <Link to="/dashboard">Subscription Management</Link>
-            <Link to="/dashboard">Push Notifications Management</Link>
-          </div>
-
-          {/* Right Section */}
-          <div className="flex-1 flex flex-col overflow-hidden">
-            {/* Secondary Navbar */}
-            <div className="h-14 bg-[#FFFBF4] px-6 flex justify-end items-center space-x-6">
-              <AiOutlineBell size={24} className="cursor-pointer" />
-              <AiOutlineUser size={26} className="cursor-pointer" />
+            {/*Side Bar Layout */}
+            <div className="w-60 border-r-2 border-gray-300 p-4 space-y-4 flex flex-col">
+                <NavLink
+                    to="/admin/dashboard"
+                    className={({ isActive }) =>
+                    `flex items-center space-x-2 px-4 py-2 rounded-md ${isActive ? "bg-[#cfedac] text-[#7B9D51]" : "text-black bg-gray-200"}`}
+                >
+                    {({ isActive }) => (
+                    <>
+                        <img
+                            src={ isActive ? Dashboard_Icon_Clicked: Dashboard_Icon_NotClicked}
+                            className="h-5"
+                        />
+                        <p> DashBoard </p>
+                    </>
+                    )}
+                </NavLink>
+                <Link to="/admin/content-management">Content Management</Link>
+                <Link to="/dashboard">Subscription Management</Link>
+                <Link to="/dashboard">Push Notifications Management</Link>
             </div>
 
-            {/* Components */}
-            <main className="flex-1 overflow-y-auto py-2 px-4">
-              <Outlet />
-            </main>
-          </div>
+            {/* Right Section */}
+            <div className="flex-1 flex flex-col overflow-hidden">
+                {/* Secondary Navbar */}
+                <div className="h-14 px-6 flex justify-end items-center space-x-6">
+                <AiOutlineBell size={24} className="cursor-pointer" />
+                <AiOutlineUser size={26} className="cursor-pointer" />
+                </div>
+
+                <main className="flex-1 overflow-y-auto py-2 px-4">
+                    <Outlet />
+                </main>
+            </div>
         </div>
 
         <Footer />
--- client/src/pages/adminDashboard/AdminDashboard.jsx
+++ client/src/pages/adminDashboard/AdminDashboard.jsx
@@ -1,9 +1,150 @@
-import React from 'react'
+import React, { useState } from "react";
+import Total_Users_Icon from "../../assets/Images/Total_Users_Icon.jpg";
+import World_Map from "../../assets/Images/World_Map.png";
 
 const AdminDashboard = () => {
-    return (
-        <div>AdminDashboard</div>
-    )
-}
+  const [active, setActive] = useState("yearly");
 
-export default AdminDashboard
+  return (
+    <>
+        <div className="flex justify-end space-x-4">
+            <button
+                onClick={() => setActive("yearly")}
+                className={`px-5 py-2 border-2 border-gray-300 rounded-md ${active === "yearly" ? "bg-[#7B9D51] text-white" : "bg-white text-black"}`}
+            >
+            Yearly
+            </button>
+            <button
+                onClick={() => setActive("monthly")}
+                className={`px-5 py-1.5 border-2 border-gray-300 rounded-md ${active === "monthly" ? "bg-[#7B9D51] text-white" : "bg-white text-black"}`}
+            >
+            Monthly
+            </button>
+            <button
+            onClick={() => setActive("weekly")}
+            className={`px-5 py-1.5 border-2 border-gray-300 rounded-md ${active === "weekly"? "bg-[#7B9D51] text-white": "bg-white text-black"}`}
+            >
+            Weekly
+            </button>
+        </div>
+
+        {/* tabs for monthly and weekly, yeraly*/}
+
+        {
+            active === "yearly" && (
+                <>
+                    <div className="flex space-x-4 mt-6 font-Inter">
+                        <div className="border-2 border-gray-300 h-30 w-78 rounded-[10px]"></div>
+                        <div className="border-2 border-gray-300 h-30 w-78 rounded-[10px]"></div>
+                        <div className="border-2 border-gray-300 h-30 w-78 rounded-[10px]"></div>
+                        <div className="border-2 border-gray-300 h-30 w-78 rounded-[10px]"></div>
+                    </div>
+
+                    <div className="flex space-x-4 mt-6 font-Inter">
+                        <div className="flex flex-col space-y-4">
+                            <div className="border-2 border-gray-300 h-20 w-78 rounded-[10px]"></div>
+                            <div className="border-2 border-gray-300 h-30.5 w-78 rounded-[10px]"></div>
+                        </div>
+                        <div className="border-2 border-gray-300 h-55 w-[40%] rounded-[10px]"></div>
+                        <div className="border-2 border-gray-300 h-55 w-[40%] rounded-[10px]"></div>
+                    </div>
+
+                    <div className="border-2 border-gray-300 w-full rounded-[10px] mt-4">
+                        <h1
+                            className="font-Inter font-semibold text-lg my-4 mx-6"
+                        >Geographic Distribution</h1>
+                        <div className="w-full flex flex-col items-center justify-center">
+                            <img 
+                                src={World_Map}
+                                className="h-40 mt-6" 
+                            />
+                            <div className="text-sm text-gray-400 text-center mt-4 mb-8">
+                                <p>World Map</p>
+                                <p>Top: US, UK, Canada, Australia</p>
+                            </div>
+                        </div>
+                    </div>
+                </>
+            )
+        }
+
+        {
+            active === "monthly" && (
+                <>
+                    <div className="flex space-x-4 mt-6 font-Inter">
+                        <div className="border-2 border-gray-300 h-30 w-78 rounded-[10px]"></div>
+                        <div className="border-2 border-gray-300 h-30 w-78 rounded-[10px]"></div>
+                        <div className="border-2 border-gray-300 h-30 w-78 rounded-[10px]"></div>
+                        <div className="border-2 border-gray-300 h-30 w-78 rounded-[10px]"></div>
+                    </div>
+
+                    <div className="flex space-x-4 mt-6 font-Inter">
+                        <div className="flex flex-col space-y-4">
+                            <div className="border-2 border-gray-300 h-20 w-78 rounded-[10px]"></div>
+                            <div className="border-2 border-gray-300 h-30.5 w-78 rounded-[10px]"></div>
+                        </div>
+                        <div className="border-2 border-gray-300 h-55 w-[40%] rounded-[10px]"></div>
+                        <div className="border-2 border-gray-300 h-55 w-[40%] rounded-[10px]"></div>
+                    </div>
+
+                    <div className="border-2 border-gray-300 w-full rounded-[10px] mt-4">
+                        <h1
+                            className="font-Inter font-semibold text-lg my-4 mx-6"
+                        >Geographic Distribution</h1>
+                        <div className="w-full flex flex-col items-center justify-center">
+                            <img 
+                                src={World_Map}
+                                className="h-40 mt-6" 
+                            />
+                            <div className="text-sm text-gray-400 text-center mt-4 mb-8">
+                                <p>World Map</p>
+                                <p>Top: US, UK, Canada, Australia</p>
+                            </div>
+                        </div>
+                    </div>
+                </>
+            )
+        }
+
+        {
+            active === "weekly" && (
+                <>
+                    <div className="flex space-x-4 mt-6 font-Inter">
+                        <div className="border-2 border-gray-300 h-30 w-78 rounded-[10px]"></div>
+                        <div className="border-2 border-gray-300 h-30 w-78 rounded-[10px]"></div>
+                        <div className="border-2 border-gray-300 h-30 w-78 rounded-[10px]"></div>
+                        <div className="border-2 border-gray-300 h-30 w-78 rounded-[10px]"></div>
+                    </div>
+
+                    <div className="flex space-x-4 mt-6 font-Inter">
+                        <div className="flex flex-col space-y-4">
+                            <div className="border-2 border-gray-300 h-20 w-78 rounded-[10px]"></div>
+                            <div className="border-2 border-gray-300 h-30.5 w-78 rounded-[10px]"></div>
+                        </div>
+                        <div className="border-2 border-gray-300 h-55 w-[40%] rounded-[10px]"></div>
+                        <div className="border-2 border-gray-300 h-55 w-[40%] rounded-[10px]"></div>
+                    </div>
+
+                    <div className="border-2 border-gray-300 w-full rounded-[10px] mt-4">
+                        <h1
+                            className="font-Inter font-semibold text-lg my-4 mx-6"
+                        >Geographic Distribution</h1>
+                        <div className="w-full flex flex-col items-center justify-center">
+                            <img 
+                                src={World_Map}
+                                className="h-40 mt-6" 
+                            />
+                            <div className="text-sm text-gray-400 text-center mt-4 mb-8">
+                                <p>World Map</p>
+                                <p>Top: US, UK, Canada, Australia</p>
+                            </div>
+                        </div>
+                    </div>
+                </>
+            )
+        }
+    </>
+  );
+};
+
+export default AdminDashboard;
