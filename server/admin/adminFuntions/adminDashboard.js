30d316199a35341670d4be48f9092a1c6b17dbaf
Zero <teja29204@gmail.com>
2025-11-20 22:52:41 +0000

    created components for the admin... todo need to fill those funtions with real code

--- server/admin/adminFuntions/adminDashboard.js
+++ server/admin/adminFuntions/adminDashboard.js
@@ -1,7 +1,74 @@
-const test = (req, res) => {
-  res.send("Admin Dashboard Functions are working");
+const adminLogin = async (req, res) => {
+  try {
+  } catch (error) {}
 };
 
+const getAdminProfile = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+
+const addVideo = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+
+const deleteVideo = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+
+const updateVideo = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+
+const addExpert = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+
+const deleteExpert = async (req, res) => {};
+
+const updateExpert = async (req, res) => {};
+
+const addEbook = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+
+const addEbooktoDraft = async (req, res) => {};
+
+const updateEbook = async (req, res) => {};
+
+const deleteEbook = async (req, res) => {};
+
+const addCategoryToList = async (req, res) => {};
+
+const removeCategoryFromList = async (req, res) => {};
+
+const addNotification = async (req, res) => {};
+
+const updateSubscriptionPlans = async (req, res) => {};
+
+const addVideoToDraft = async (req, res) => {};
+
 module.exports = {
-  test,
+  adminLogin,
+  getAdminProfile,
+  addVideo,
+  deleteVideo,
+  updateVideo,
+  addExpert,
+  deleteExpert,
+  updateExpert,
+  addEbook,
+  addEbooktoDraft,
+  updateEbook,
+  deleteEbook,
+  addCategoryToList,
+  removeCategoryFromList,
+  addNotification,
+  updateSubscriptionPlans,
+  addVideoToDraft,
 };
---
+++ server/admin/adminFuntions/adminGetComponents.js
@@ -0,0 +1,53 @@
+const getAllUsers = async (req, res) => {};
+
+const getAllVideos = async (req, res) => {};
+
+const getAllExperts = async (req, res) => {};
+
+const getAllEbooks = async (req, res) => {};
+
+const getAllCategories = async (req, res) => {};
+
+const getAllNotifications = async (req, res) => {};
+
+const getDashboardStats = async (req, res) => {};
+
+const getSubscriptionPlans = async (req, res) => {};
+
+const getAllDraftVideos = async (req, res) => {};
+
+const getAllDraftEbooks = async (req, res) => {};
+
+//////////
+//////////
+//////////
+//////////
+
+////TODO: IMPLEMENT GET INDIVIDUAL DATA RETRIVING FUNTIONS FOR DASHBOARD
+
+//////////
+//////////
+//////////
+//////////
+
+const getSingleVideo = async (req, res) => {};
+
+const getSingleExpert = async (req, res) => {};
+
+const getSingleEbook = async (req, res) => {};
+
+module.exports = {
+  getAllUsers,
+  getAllVideos,
+  getAllExperts,
+  getAllEbooks,
+  getAllCategories,
+  getAllNotifications,
+  getDashboardStats,
+  getSubscriptionPlans,
+  getAllDraftVideos,
+  getAllDraftEbooks,
+  getSingleVideo,
+  getSingleExpert,
+  getSingleEbook,
+};
